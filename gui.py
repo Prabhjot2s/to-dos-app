a3ed7c574372f692aca6f82bf7b7e20e7e7a4b9a
prabh <pschhabra22@gmail.com>
2023-06-25 00:51:59 -0400

    handled all the errors added time june 25th

--- gui.py
+++ gui.py
@@ -1,8 +1,12 @@
 import function
 import PySimpleGUI as py
+import time
+
+
 
 
 label=py.Text("To-do-list")
+labels=py.Text("",key="Clock")
 #Input box
 input_box=py.InputText(tooltip="Enter Todo",key='todo')
 list_box=py.Listbox(values=function.openfile(),key='todos',enable_events=True,size=[45,10])
@@ -13,14 +17,15 @@ compelte=py.Button('Complete')
 edit=py.Button("Edit")
 exit=py.Button("Exit")
 #Layout window
-window=py.Window('App',layout=[[label],[input_box,button],[compelte,list_box,edit],[exit]]
+window=py.Window('App',layout=[[label,labels],[input_box,button],[compelte,list_box,edit],[exit]]
                  ,font=('Times new roman',20))
 
 
 while True:
 
-    value=window.read()
+    value=window.read(timeout=200)
     print(value)
+    window['Clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
 
     if value[0] == 'Add':
         file_value = function.openfile()
@@ -29,25 +34,36 @@ while True:
         window['todos'].update(file_value)
 
     elif value[0] == 'Complete':
-        to_remove = value[1]['todos'][0]
-        vari = function.openfile()
-        vari.remove(to_remove)
-        function.writeline(vari)
-        window['todos'].update(values=vari)
-        window['todo'].update(value='')
+        try:
+            to_remove = value[1]['todos'][0]
+            vari = function.openfile()
+            vari.remove(to_remove)
+            function.writeline(vari)
+            window['todos'].update(values=vari)
+            window['todo'].update(value='')
+        except IndexError:
+            py.popup('Select a task to compelte',title="Error",font=20)
 
     elif value[0] == 'Edit':
+        try:
 
-            todos=value[1]['todos'][0]
-            new_todo=value[1]['todo']+'\n'
-            values=function.openfile()
-            index=values.index(todos)
-            values[index]=new_todo
-            function.writeline(values)
-            window['todos'].update(values=values)
+                todos=value[1]['todos'][0]
+                new_todo=value[1]['todo']+'\n'
+                values=function.openfile()
+                index=values.index(todos)
+                values[index]=new_todo
+                function.writeline(values)
+                window['todos'].update(values=values)
+        except IndexError:
+            py.popup("Select Something First To Edit ",font=20,title="Error")
+
+
 
     elif value[0]=='todos' :
-      window['todo'].update(value[1]['todos'][0])
+     try:
+        window['todo'].update(value[1]['todos'][0])
+     except IndexError:
+        continue
 
     elif value[0]=='Exit':
         break
